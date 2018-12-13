e8e22c5bf47c63e95c90821d3eba6263ba515ed7
frantsmn-work <frantsmn@gmail.com>
2018-12-13 16:13:52 +0300

    +css fixes (adaptivity)

--- neon-buttons/neon-buttons.js
+++ neon-buttons/neon-buttons.js
@@ -7,32 +7,23 @@ document.querySelectorAll(".neon").forEach((element) => {
 function neon(element) {
     let skew;
     let interval;
-
-    var x = new Audio('/neon-buttons/btn.wav');
-    x.volume = 0.1;
+    let delay;
+    let fx = new Audio('/neon-buttons/btn.wav');
+    fx.volume = 0.1;
 
     element.addEventListener('mouseover', start);
-    element.addEventListener('touchstart', start);
-
     element.addEventListener('mousemove', checkCursor);
+    element.addEventListener('mouseout', resetStyle);
+
+    // element.addEventListener('touchend', resetStyle);
 
     function start() {
-        x.play();
-
+        element.classList.add('active');
         element.style.transition = `.3s ease-out transform`
-        let startDelay = setTimeout(() => {
-            interval = setInterval(() => element.style.transform = `perspective(${skew.size}px) rotate3d(${skew.y},${skew.x},0,${skew.gipoten}deg)`, 30);
-        }, 0);
+        fx.play();
 
-        let resetStyle = () => {
-            element.style.transition = `.5s ease-in transform`;
-            element.style.transform = `perspective(0) rotate3d(0,0,0,0)`;
-            element.blur();
-            clearInterval(interval);
-            clearTimeout(startDelay);
-        }
-        element.addEventListener('mouseout', resetStyle);
-        element.addEventListener('touchend', resetStyle);
+        interval = setInterval(() => element.style.transform = `perspective(${skew.size}px) rotate3d(${skew.y},${skew.x},0,${skew.gipoten}deg)`, 30);
+
     }
 
     function checkCursor(e) {
@@ -47,5 +38,13 @@ function neon(element) {
             y: (-pos.y / this.offsetHeight * 10) | 0,
             gipoten: (Math.sqrt(pos.x * pos.x + pos.y * pos.y) / 4) | 0
         }
+    }
+
+    function resetStyle() {
+        element.blur();
+        element.classList.remove('active');
+        element.style.transition = `.5s ease-in transform`;
+        element.style.transform = `perspective(0) rotate3d(0,0,0,0)`;
+        clearInterval(interval);
     }
 }
