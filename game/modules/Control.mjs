e89f084e7a759ab650b0a1856e88356a4db558f9
frantsmn-work <frantsmn@gmail.com>
2018-12-18 18:18:06 +0300

    +Control lagging fix

--- game/modules/Control.mjs
+++ game/modules/Control.mjs
@@ -1,6 +1,8 @@
 export default class Control {
     constructor(controller, block) {
 
+        //Флаг для регирования на нажатия (true = реагировать | false = не реагировать )
+        let controlAvailable = true;
         //ID таймаута для повторений движения влево/вправо блока, при зажатой кнопке
         let shiftRepeatTimeoutID = 0;
         //ID таймаута для повторений движения вниз блока, при зажатой кнопке
@@ -21,127 +23,134 @@ export default class Control {
             'button-down': 'Down'
         }
 
-        this.stopListenKeyboard = function stopListenKeyboard() {
-            document.removeEventListener('keydown', this.keydown);
-            document.removeEventListener('keyup', this.keyup);
-            controller.querySelectorAll('button').forEach((button) => {
-                button.removeEventListener('touchstart', this.keydown);
-                button.removeEventListener('touchend', this.keyup);
-            });
-        }
-
-        this.startListenKeyboard = function startListenKeyboard() {
-            document.addEventListener('keydown', this.keydown);
-            document.addEventListener('keyup', this.keyup);
-            controller.querySelectorAll('button').forEach((button) => {
-                button.addEventListener('touchstart', this.keydown);
-                button.addEventListener('touchend', this.keyup);
-            });
-        }
-
         this.keydown = (e) => {
+            if (controlAvailable) {
 
-            //Если такая кнопка уже нажата, то избавляемся от системных повторов нажатия (система клацает сама, при зажатии клавиши)
-            if (this.key[voc[e.keyCode || e.srcElement.id]]) return;
-            //Сохранить в свойство объекта key, какая кнопка на калавиатуре нажата
-            this.key[voc[e.keyCode || e.srcElement.id]] = true;
-            //Применение стилей для нажатой тач-кнопки
-            e.srcElement.classList.add('active');
+                //Если такая кнопка уже нажата, то избавляемся от системных повторов нажатия (система клацает сама, при зажатии клавиши)
+                if (this.key[voc[e.keyCode || e.srcElement.id]]) return;
+                //Сохранить в свойство объекта key, какая кнопка на калавиатуре нажата
+                this.key[voc[e.keyCode || e.srcElement.id]] = true;
+                //Применение стилей для нажатой тач-кнопки
+                e.srcElement.classList.add('active');
 
-            // console.log('keydown: this.key = ', this.key, ' code: ', e.keyCode);
+                // console.log('keydown: this.key = ', this.key, ' code: ', e.keyCode);
 
-            switch (voc[e.keyCode || e.srcElement.id]) {
-                case 'B':
-                    block.activeBlock.rotateLeft();
-                    return;
-                case 'A':
-                    block.activeBlock.rotateRight();
-                    return;
-                case 'Right':
-                    clearTimeout(shiftRepeatTimeoutID); //Обнуляем повторы нажатия чтобы не возникал кнфликт при одновременном нажатии
-                    block.activeBlock.moveRight();
-                    shiftRepeatTimeoutID = setTimeout(function tick() {
+                switch (voc[e.keyCode || e.srcElement.id]) {
+                    case 'B':
+                        block.activeBlock.rotateLeft();
+                        return;
+                    case 'A':
+                        block.activeBlock.rotateRight();
+                        return;
+                    case 'Right':
+                        clearTimeout(shiftRepeatTimeoutID); //Обнуляем повторы нажатия чтобы не возникал кнфликт при одновременном нажатии
                         block.activeBlock.moveRight();
-                        //Рекурсивно вызываем таймаут
-                        shiftRepeatTimeoutID = setTimeout(tick, 100);
-                    }, 267);
-                    break;
-                case 'Left':
-                    clearTimeout(shiftRepeatTimeoutID); //Обнуляем повторы нажатия чтобы не возникал кнфликт при одновременном нажатии
-                    block.activeBlock.moveLeft();
-                    shiftRepeatTimeoutID = setTimeout(function tick() {
+                        shiftRepeatTimeoutID = setTimeout(function tick() {
+                            block.activeBlock.moveRight();
+                            //Рекурсивно вызываем таймаут
+                            shiftRepeatTimeoutID = setTimeout(tick, 100);
+                        }, 267);
+                        break;
+                    case 'Left':
+                        clearTimeout(shiftRepeatTimeoutID); //Обнуляем повторы нажатия чтобы не возникал кнфликт при одновременном нажатии
                         block.activeBlock.moveLeft();
-                        //Рекурсивно вызываем таймаут
-                        shiftRepeatTimeoutID = setTimeout(tick, 100);
-                    }, 267);
-                    break;
-                case 'Down':
-                    setTimeout(()=> {clearTimeout(shiftRepeatTimeoutID); console.log('cleared before!')},0);
-                    downRepeatTimeoutID = setTimeout(function tick() {
-                        block.activeBlock.moveDown();
-                        //Сообщаем о нажатой клавише вниз (необходимо для подсчета строк, которые пролетит блок)
-                        EMITTER.emit('control:downPressed', true);
-                        //Рекурсивно вызываем таймаут
-                        downRepeatTimeoutID = setTimeout(tick, 37);
-                    }, 0);
-                    break;
-                default:
-                    break;
+                        shiftRepeatTimeoutID = setTimeout(function tick() {
+                            block.activeBlock.moveLeft();
+                            //Рекурсивно вызываем таймаут
+                            shiftRepeatTimeoutID = setTimeout(tick, 100);
+                        }, 267);
+                        break;
+                    case 'Down':
+                        // setTimeout(() => {
+                        //     clearTimeout(shiftRepeatTimeoutID);
+                        //     console.log('cleared before!')
+                        // }, 0);
+                        downRepeatTimeoutID = setTimeout(function tick() {
+                            block.activeBlock.moveDown();
+                            //Сообщаем о нажатой клавише вниз (необходимо для подсчета строк, которые пролетит блок)
+                            EMITTER.emit('control:downPressed', true);
+                            //Рекурсивно вызываем таймаут
+                            downRepeatTimeoutID = setTimeout(tick, 37);
+                        }, 0);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                //Даже если контролллер заблокирован, необходимо отслеживать последние нажатые клавиши
+                //чтобы после разблокировки контроллера, не срабатвыала заранее зажатая клавиша
+
+                //Сохранить в свойство объекта key, какая кнопка на калавиатуре нажата
+                this.key[voc[e.keyCode || e.srcElement.id]] = true;
             }
         }
 
-        
         this.keyup = (e) => {
 
             delete this.key[voc[e.keyCode || e.srcElement.id]];
 
-            // setTimeout(, 0);
-            //Если были нажаты кнопки поворота, то не стоит сбрасывать интервалы повтора перемещений,
-            //иначе движение фигуры по зажатой кнопке (влево/вправо), прекратится
             switch (voc[e.keyCode || e.srcElement.id]) {
-                // case 'B':
-                // case 'A':
-                //     return;
                 case 'Down':
                     //Сообщаем об отпущенной клавише "down" (необходимо для подсчета строк, за которые дропнется блок)
                     EMITTER.emit('control:downPressed', false);
-                    //Очищаем интервал
                     setTimeout(clearTimeout(downRepeatTimeoutID), 0);
                     break;
                 case 'Left':
                 case 'Right':
                     setTimeout(clearTimeout(shiftRepeatTimeoutID), 0);
-                    
                     break;
                 default:
                     break;
             }
 
-            // console.log('keyup: this.key = ', this.key);
-
-            //Стили для отпущенной тач-кнопки
+            //Применить стили для отпущенной тач-кнопки
             e.srcElement.classList.remove('active');
+
         }
 
-        this.startListenKeyboard(); //?
-
-        // EMITTER.subscribe('canvas:wipeAnimationStart', () => this.stopListenKeyboard()); //Блокируем управление во время анимации
-        // EMITTER.subscribe('canvas:wipeAnimationEnd', () => this.startListenKeyboard()); //Разблокируем управление после анимации
-        EMITTER.subscribe('block:gameOver', () => {
-            //Блокируем управление по gameover
-            this.stopListenKeyboard();
+        const controlLock = () => {
+            controlAvailable = false;
             //Вне очереди очищаем рекурсивный таймаут на повтор движения в стороны
-            setTimeout(() => clearTimeout(shiftRepeatTimeoutID), 0);
+            // setTimeout(() => clearTimeout(shiftRepeatTimeoutID), 0);
             //Вне очереди очищаем рекурсивный таймаут на повтор движения вниз
-            setTimeout(() => clearTimeout(downRepeatTimeoutID), 0);
+            // setTimeout(() => clearTimeout(downRepeatTimeoutID), 0);
+
+            window.requestAnimationFrame(() => {
+                clearTimeout(downRepeatTimeoutID);
+                // console.log('downRepeatTimeoutID cleared!');
+            });
+
+            window.requestAnimationFrame(() => {
+                clearTimeout(shiftRepeatTimeoutID);
+                // console.log('downRepeatTimeoutID cleared!');
+            });
+            // console.log('control available: ', controlAvailable);
+        }
+
+        const controlUnlock = () => {
+            controlAvailable = true;
+            // console.log('control available: ', controlAvailable);
+        }
+
+        //Вешаем обработчики на кнопки
+        document.addEventListener('keydown', this.keydown);
+        document.addEventListener('keyup', this.keyup);
+        controller.querySelectorAll('button').forEach((button) => {
+            button.addEventListener('touchstart', this.keydown);
+            button.addEventListener('touchend', this.keyup);
         });
+
+        EMITTER.subscribe('canvas:wipeAnimationStart', () => controlLock()); //Блокируем управление во время анимации
+        EMITTER.subscribe('canvas:wipeAnimationEnd', () => controlUnlock()); //Разблокируем управление после анимации
+        EMITTER.subscribe('block:gameOver', () => controlLock()); //Блокируем управление по gameover
         EMITTER.subscribe('block:blockFixed', () => {
             //Вне очереди очищаем рекурсивный таймаут на повтор движения вниз
             //Таким образом запрещаем блоку сразу падать после появления
             window.requestAnimationFrame(() => {
-                clearTimeout(downRepeatTimeoutID); console.log('downRepeatTimeoutID cleared!');
+                clearTimeout(downRepeatTimeoutID);
+                // console.log('downRepeatTimeoutID cleared!');
             });
-            // setTimeout(() => clearTimeout(downRepeatTimeoutID), 0);
         });
+
     }
 }
--- game/tetris.mjs
+++ game/tetris.mjs
@@ -2,27 +2,26 @@ import Matrix from './modules/Matrix.mjs';      //М
 import Block from './modules/Block.mjs';        //M
 import Ticker from './modules/Ticker.mjs';      //M
 
-import Stats from './modules/Stats.mjs';        //MV
-
-import Canvas from './modules/Canvas.mjs';      //V
-import Textures from './modules/Textures.mjs';  //V
 import Sound from './modules/Sound.mjs';        //V
+import Textures from './modules/Textures.mjs';  //V
+import Canvas from './modules/Canvas.mjs';      //V
+import Stats from './modules/Stats.mjs';        //V
 
 import Control from './modules/Control.mjs';    //C
 
 //=======================================================
 
-let sound = new Sound();
 let textures = new Textures();
 let canvas = new Canvas(document.getElementsByTagName('canvas')[0], textures);
+let sound = new Sound();
 let stats = new Stats(document.getElementsByClassName('game')[0], sound);
 
-let matrix = new Matrix('[[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,{"state":"fixed","color":3},null,null,null,null,null,null],[null,null,{"state":"fixed","color":3},{"state":"fixed","color":3},null,null,null,null,null,null],[null,null,{"state":"fixed","color":3},{"state":"fixed","color":3},null,null,null,null,null,null],[null,{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},null,{"state":"fixed","color":3},{"state":"fixed","color":3},null,null,null],[null,{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":1},{"state":"fixed","color":1},{"state":"fixed","color":3},null,null,null],[null,{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":1},{"state":"fixed","color":1},{"state":"fixed","color":3},null,null,null],[{"state":"fixed","color":2},{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},null],[{"state":"fixed","color":2},{"state":"fixed","color":2},{"state":"fixed","color":2},null,{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3}]]');
 //let matrix = new Matrix();
+let matrix = new Matrix('[[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null,null],[null,null,null,{"state":"fixed","color":3},null,null,null,null,null,null],[null,null,{"state":"fixed","color":3},{"state":"fixed","color":3},null,null,null,null,null,null],[null,null,{"state":"fixed","color":3},{"state":"fixed","color":3},null,null,null,null,null,null],[null,{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},null,{"state":"fixed","color":3},{"state":"fixed","color":3},null,null,null],[null,{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":1},{"state":"fixed","color":1},{"state":"fixed","color":3},null,null,null],[null,{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":1},{"state":"fixed","color":1},{"state":"fixed","color":3},null,null,null],[{"state":"fixed","color":2},{"state":"fixed","color":1},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},null],[{"state":"fixed","color":2},{"state":"fixed","color":2},{"state":"fixed","color":2},null,{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3},{"state":"fixed","color":3}]]');
 let block = new Block(matrix, canvas, stats, sound); //Принимает Matrix, Canvas, Stats, и queue (последовательность блоков) arr[1..7*1000]
+let ticker = new Ticker(block);
 
 let control = new Control(document.getElementById('controller'), block);
-let ticker = new Ticker(block);
 
 EMITTER.subscribe('textures:ready', () => {
     initGame();
